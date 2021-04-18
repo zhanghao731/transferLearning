5fb3dbda22bfe6fb522c419d3a6f35ec4c6e2793
zhanghao <986136244@qq.com>
2021-04-18 23:19:28 +0800

    test

--- test.py
+++ test.py
@@ -4,3 +4,12 @@ print(torch.cuda.is_available())
 from torch.backends import cudnn
 
 print(cudnn.is_available())
+x = torch.tensor(1, device = 'cuda')
+# print(x)
+# dev = torch.device('cuda:0')
+# x.to(dev)
+print(x)
+# print(dev)
+print(torch.cuda.device_count())
+# print(torch.version.cuda)
+print(torch.cuda.get_device_name(0))
